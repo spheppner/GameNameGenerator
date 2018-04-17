c307ada805c5fc6f5fb60b7a4b6514fd61ab97bb
Simon Heppner <hasibandi@gmx.at>
2018-04-17 17:01:16 +0200

    upgraded the printing of the names

--- nameGameGenerator.py
+++ nameGameGenerator.py
@@ -6,6 +6,20 @@ word_2 = ["Game", "Program", "Battle", "Python Game", "Battle Simulator", "Tourn
 word_3 = ["for", "without", "of"]
 word_4 = ["drunken dwarfs", "drunken trolls", "humans", "artifficial intelligences", "gamers", "programmers", "nerds", "drunken elfs", "drunken lizards", "Dumb Lizards", "dumb and drunken trolls", "dumb and drunken elfs", "soup-sucking, dumb and drunken elfs", "soup-sucking robots with creepy red eyes", "maniacly laughing robots with laser-shooting eyes", "maniacly laughing elfs with red eyes, pistols and pink unicorn armor"]
 
-for x in range(5):
+while True:
+	hm = input("How many names do you want to have? > ")
+	try:
+		val = int(hm)
+	except ValueError:
+		continue
+	if hm == "":
+		continue
+	elif hm != "":
+		break
+
+print("-----------------------------------------------------------")
+for x in range(int(hm)):
 	name = random.choice(adjectives) + " " + random.choice(word_2) + " " + random.choice(word_3) + " " + random.choice(word_4)
-	print(name)
+	print(str(x) + ": " + name)
+	print("***")
+print("-----------------------------------------------------------")
